c9cac3a6570246c50f19047c0a4154dbe8bc9bbd
mganeko <masashi.g@gmail.com>
2021-04-26 23:25:10 +0900

    stack machine

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -213,6 +213,36 @@ Node *expr() {
   }
 }
 
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+  case ND_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  case ND_MUL:
+    printf("  imul rax, rdi\n");
+    break;
+  case ND_DIV:
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
+  }
+
+  printf("  push rax\n");
+}
 
 // --- node ---
 
